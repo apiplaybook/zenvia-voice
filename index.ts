5fb94df3ef0bb35880ff989d630c9d16e1fc6c5f
Henry Kimura <htkimura@gmail.com>
2021-01-15 14:40:57 -0300

    created the express api

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hello!')
+import express from 'express'
+import { handleSendCode } from './total-voice/index'
+
+const app = express()
+
+app.get('/sendCode/:phone', handleSendCode)
+
+app.listen(4000, () => console.log('Servidor rodando na porta 4000.'))
